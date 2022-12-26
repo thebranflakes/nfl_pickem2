80c8110977715f224b5dfb7842089413ac473992
thebranflakes <hernanb2@oregonstate.edu>
2022-12-26 17:43:12 -0500

    Starting a new setup for nfl pickem. Just starting up some new flask and jinja pages.

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
+from flask import Flask, render_template
 
+# Create Flask Instance
+app = Flask(__name__)
+
+
+# FILTERS:
+# safe
+# capitalize
+# lower
+# upper
+# title
+# trim
+# striptags
+
+
+# Create a route
+@app.route('/')
+def index():
+    nfl_teams = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", "GB", "HOU", "IND",
+                 "JAX", "KC", "LAC", "LAR", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "LV", "PHI", "PIT", "SEA", "SF",
+                 "TB", "TEN", "WAS"]
+    return render_template('index.html', nfl_teams=nfl_teams)
+
+
+# localhost:5000/user/John
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+# Create Custom Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
