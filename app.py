bc04c9db72c4fcb896bc985b06622e69af558fd1
thebranflakes <hernanb2@oregonstate.edu>
2023-01-02 15:54:44 -0500

    what the forms install, need to set up virtual environment

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
 
 # Create Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "69_secret_key_420"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("Whats Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # FILTERS:
@@ -38,3 +49,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
+
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html', name=name, form=form)
